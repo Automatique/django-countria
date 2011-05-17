f889dde6155f61b34825347ba6e1b5505853d6c6
Tijs Teulings <tteulings@gmail.com>
2011-05-17 14:39:45 +0200

    removed fields i don't need, and replaced initial data fixture

--- README.txt
+++ README.txt
@@ -1,10 +1,11 @@
-===============
-Django Countria
-===============
+=====================
+Django Countria Light
+=====================
 
 A full collection of countries, cities, currency, continents and states for Django.
 
-Infomation like calling code, top level domain, latitude, longtitude, population, capital city, etc.
+This is a fork of django-countria which removes translations and related dependencies. Information like calling code,
+top level domain, population and capital city is also removed.
 
 - Approximately 270 Countries
 - Approximately 190,000 Cities
@@ -12,5 +13,6 @@ Infomation like calling code, top level domain, latitude, longtitude, population
 - Approximately 4200 States
 
 Source:
+https://github.com/geomin/django-countria
 http://geolite.maxmind.com/download/worldcities/
 http://www.andrewpatton.com/countrylist.html
--- countria/models.py
+++ countria/models.py
@@ -26,22 +26,11 @@ class Country(models.Model):
     name            = models.CharField(max_length=64, unique=True)
     full_name       = models.CharField(max_length=64)
     currency        = models.ForeignKey('Currency', null=True)
-    sovereignty     = models.ForeignKey('self', null=True)
-    capital         = models.ForeignKey('City', null=True, related_name="country_capital")   
-    idc             = models.PositiveIntegerField(null=True) # International Dialing Code
     iso_2           = models.CharField(max_length=2, null=True)
     iso_3           = models.CharField(max_length=3, null=True)
-    iso_number      = models.PositiveIntegerField(null=True)
-    tld             = models.CharField(max_length=7, null=True)
     latitude        = models.DecimalField(max_digits=9, decimal_places=6, default=Decimal("0.0"))
     longitude       = models.DecimalField(max_digits=9, decimal_places=6, default=Decimal("0.0"))
-    population      = models.PositiveIntegerField(null=True)
-    continent       = models.ForeignKey(Continent, null=True)
-
-    def __calling_code(self):
-        return '00%d' % self.idc
-
-    calling_code = property(__calling_code)
+    continent       = models.ForeignKey('Continent', null=True)
 
     def __unicode__(self):
         if hasattr(settings, 'MAX_COUNTRY_NAME_LENGTH'):
@@ -53,7 +42,6 @@ class City(models.Model):
     name = models.CharField(max_length=64)
     country     = models.ForeignKey('Country', null=True)
     state       = models.ForeignKey('State', null=True)
-    population  = models.PositiveIntegerField(null=True)
     latitude    = models.DecimalField(max_digits=9, decimal_places=6, default=Decimal("0.0"))
     longitude   = models.DecimalField(max_digits=9, decimal_places=6, default=Decimal("0.0"))
 
@@ -62,7 +50,6 @@ class City(models.Model):
 
 class State(models.Model):
     name = models.CharField(max_length=64)
-    capital     = models.ForeignKey('City', related_name="state_capital", null=True)
     country     = models.ForeignKey('Country', related_name="state_country", null=True)
     latitude    = models.DecimalField(max_digits=9, decimal_places=6, default=Decimal("0.0"))
     longitude   = models.DecimalField(max_digits=9, decimal_places=6, default=Decimal("0.0"))
